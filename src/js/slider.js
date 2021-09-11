4a9059a3dc1b0c683ed3e69a2bebd2ccc6513e49
Piron-Johny <piron.johny@gmail.comm>
2021-09-11 11:31:14 +0300

    Добавил разметку. Подключил JQuery. Папку JS.

---
+++ src/js/slider.js
@@ -0,0 +1,11 @@
+// slider
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    arrows: true,
+  });
+});
